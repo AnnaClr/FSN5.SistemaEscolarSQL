8574bb81c4af9d232d7c2ec8f29ddd0b92f11e58
Annie / <116754500+AnnaClr@users.noreply.github.com>
2025-02-18 18:53:35 -0300

    Update schoolsystem.sql

--- schoolsystem.sql
+++ schoolsystem.sql
@@ -33,26 +33,26 @@ CREATE TABLE matriculas (
 INSERT INTO professores (nome, departamento) VALUES
 ('João Silva', 'Matemática'),
 ('Maria Oliveira', 'Ciências'),
-('Carlos Souza', 'História');
+('José Andrade', 'História');
 
 -- DADOS DAS DISCIPLINAS:
 INSERT INTO disciplinas (nome, carga_horaria, professor_id) VALUES
-('Matemática Básica', 60, 1),
+('Matemática', 60, 1),
 ('Física', 80, 2),
-('História do Brasil', 40, 3);
+('História', 40, 3);
 
 -- DADOS DOS ALUNOS:
 INSERT INTO alunos (nome, data_nascimento) VALUES
-('Ana Costa', '2005-03-15'),
-('Pedro Santos', '2006-07-22'),
-('Lucas Lima', '2005-11-30');
+('Ana Vieira', '2005-08-09'),
+('Lucas Santos', '2006-08-22'),
+('Kaio Lima', '2005-12-05');
 
 -- DADOS DAS MATRICULAS:
 INSERT INTO matriculas (aluno_id, disciplina_id, data_matricula) VALUES
-(1, 1, '2023-01-10'),
-(1, 2, '2023-01-10'),
-(2, 1, '2023-01-11'),
-(3, 3, '2023-01-12');
+(1, 1, '2025-01-10'),
+(1, 2, '2025-01-11'),
+(2, 1, '2025-01-12'),
+(3, 3, '2025-01-13');
 
 SELECT * FROM professores;
 SELECT * FROM disciplinas;
